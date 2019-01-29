7b93f77ff80faaa315de0960e2db6ca618809474
Dennis De Vetter <dennisdevetter@gmail.com>
2019-01-29 20:28:08 +0100

    foo

--- Laravel/Projects/frozengraphics/resources/views/layouts/app.blade.php
+++ Laravel/Projects/frozengraphics/resources/views/layouts/app.blade.php
@@ -4,11 +4,8 @@
     <meta charset="UTF-8">
 
     <!-- Favicons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/svg/safari-pinned-tab.svg" color="#5bbad5">
 
     <!-- meta keys -->
     <meta name="msapplication-TileColor" content="#da532c">
