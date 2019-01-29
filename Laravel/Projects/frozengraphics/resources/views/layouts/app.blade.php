0046c0bf34f7f8bf125c6c8572183ef2849a939c
Dennis De Vetter <dennisdevetter@gmail.com>
2019-01-29 20:21:05 +0100

    favicons

--- Laravel/Projects/frozengraphics/resources/views/layouts/app.blade.php
+++ Laravel/Projects/frozengraphics/resources/views/layouts/app.blade.php
@@ -2,11 +2,23 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/svg/safari-pinned-tab.svg" color="#5bbad5">
+
+    <!-- meta keys -->
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'frozengraphics.be')</title>
+    <!-- Web title -->
+    <title>@yield('title', 'frozengraphics.be - IT Consultancy')</title>
 
     <!-- Fonts -->
     <!--<link rel="stylesheet" type="text.css" href="https://fonts.googleapis.com/css?family=Nunito:200,600"/>-->
@@ -15,6 +27,7 @@
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}"/>
 </head>
 <body>
+    <!-- App -->
     <div id="app" class="flex-center position-ref full-height">
         @yield('content')
     </div>
